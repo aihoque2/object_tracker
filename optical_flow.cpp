38488fb25af89b1491610b94934ffefe4bd3f11e
aihoque2 <aihoque2@illinois.edu>
2020-09-09 20:39:54 -0400

    edited things

--- optical_flow.cpp
+++ optical_flow.cpp
@@ -4,18 +4,20 @@
  */
 
 #include <opencv2/highgui.hpp>
-#include <opencv2/imageproc.hpp>
-
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
 using namespace cv;
 using namespace std;
-
 
 
 Mat process_division(Mat image){
         //note: this takes in 1-channel images for input
         Mat gray, smooth, dst;
 
-        cvtColor(image, gray, BGR2GRAY);// gray in case of emergency
+        //cvtColor(image, gray, CV_BGR2GRAY);// gray in case of emergency
 
         Mat kernel = getStructuringElement(MORPH_RECT, Size(5,5));
 
@@ -28,48 +30,66 @@ Mat process_division(Mat image){
 }
 
 
-Mat get_corners(Mat Image){ //1-channel image for input
+vector<Point2f> get_corners(Mat image){ //1 channel image will do
 	/* return an image that shows all the corners detected 
 	 * utilizes the goodFeaturesToTrack() function*/
 
 	//shi-tomasi parameters
-	vector<Point2f> corners;
+	
+
+	vector<Point2f> corners; //our return
 	double qualityLevel = 0.01;
 	double minDistance = 10;
 	int blockSize = 3;
 	bool useHarrisDetector = false;
 	double k = 0.04;
+	int maxCorners = 300;
+
+	Mat div = process_division(image);
 	
-	Mat copy = Image.clone() //Deep copy? of the thing
+	goodFeaturesToTrack(div,
+               corners,
+               maxCorners,
+               qualityLevel,
+               minDistance,
+               Mat(),
+               blockSize,
+               useHarrisDetector,
+               k );
 
-	goodFeaturesToTrack(Image, corners, 100, qualityLevel, minDistance, blockSize, useHarrisDetector, k);
+	return corners;
+
 	
-	for (int i = 0; i < corners.size(); i++){
-		circle(copy, corners[i], r, Scalar(),( rng.uniform(), rng.uniform(), rng.uniform()), -1, 8, 0);
-	}
-
-	return copy
-
 }
-
 
 
 int main(){
 
 	string filename = "data/images/Test21_1.tif";
-	Mat div, cornerImg;	
+	Mat div, copy;	
 
 	Mat image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
 	if (!image.data){
 		cout << "ur image sux" << endl;
 		return -1;
 	}
-
-	div = process_division(image);
-	cornerImg = get_corners(div)
-
-	namedWindow("Display Window", WINDOW_AUTOSIZE);
 	
 
+	vector<Point2f> corners = get_corners(image);
+	 
+	copy = image.clone();
+	
+	int r = 4;
 
+	for( int i = 0; i < corners.size(); i++ ){ 
+	circle( copy, corners[i], r, Scalar(255, 0, 0), -1, 8, 0 );
+
+	
+	}
+
+	namedWindow("Display Window", WINDOW_AUTOSIZE);
+	imshow("Display Window", copy);
+	waitKey(0);
+	return 0;
 }
+
